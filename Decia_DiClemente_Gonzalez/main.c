5b88e41e85c74fd226d564fc86da798043293cb8
IvanDiClemente <ivandiclementejesus@gmail.com>
2022-06-21 18:07:03 -0300

    Revert "Clientes ordenados por Insercion"
    This reverts commit ad7cd695899215566dbe976280c8edf74d0cb159.

--- Decia_DiClemente_Gonzalez/main.c
+++ Decia_DiClemente_Gonzalez/main.c
@@ -54,16 +54,7 @@ void cargaDeDomicilio(stCliente cliente);
 void cargaDeClientes(stCliente cliente);
 void bajaDeClientes(char arregloChar[]);
 void modificarCliente (char archivo[]);
-<<<<<<< Updated upstream
 void modificarDatosCliente (char archivo[],int opcion);
-=======
-stCliente modificarApellido(stCliente cliente);
-stCliente modificarNombre(stCliente cliente);
-stCliente modificarMovil(stCliente cliente);
-stDomicilio modificardomicilio(stDomicilio domicilio);
-stCliente modificarEmail(stCliente cliente);
-void ordenamientoPorInsercionNombreApellido(char arregloChar[], int validos);
->>>>>>> Stashed changes
 
 
 
@@ -412,7 +403,6 @@ void modificarCliente (char archivo[])
 
                         scanf("%i", &opcion);
 
-<<<<<<< Updated upstream
                         modificarDatosCliente(archivo,opcion);
 
                         rewind(buff);
@@ -450,10 +440,6 @@ void modificarDatosCliente (char archivo[],int opcion){
 
     if(archivo!=NULL){
         switch(opcion)
-=======
-
-                        switch(opcion)
->>>>>>> Stashed changes
                         {
 
                         case 1:
@@ -701,7 +687,6 @@ int BuscarPosMenor (stCliente cliente[],int pos,int validos){
 }
 
 void ordenamientoPorSeleccionClientes (stCliente clientes[],int validos){
-
     int i=0;
     int pmenor;
     stCliente aux;
@@ -717,35 +702,28 @@ void ordenamientoPorSeleccionClientes (stCliente clientes[],int validos){
 
 //Pasar archivo a arreglo
 int pasarArchivoToArreglo (char nombreArch[],stCliente clientes[],int dimension){
-
     FILE * buf=fopen(nombreArch,"rb");
     stCliente aux;
 
     int i =0;
 
     if(buf){
-
         while((fread(&aux,sizeof(stCliente),1,buf)>0)&& i<dimension){
 
                 clientes[i]=aux;
                 i++;
-
         }
-
         fclose(buf);
-
     }
     return i;
 }
 
 //Pasar arreglo a archivo
 void pasarArregloToArchivo (char nombreArch[],stCliente clientes[],int validos){
-
     FILE * archi=fopen(nombreArch,"ab");
     int i=0;
 
     if(archi!=NULL){
-
         i=fwrite(clientes,sizeof(stCliente),1,archi);
 
         fclose(archi);
@@ -754,91 +732,3 @@ void pasarArregloToArchivo (char nombreArch[],stCliente clientes[],int validos){
 }
 
 //Ordenados por método de inserción tomando en cuenta nombre Y apellido (ambos al mismo tiempo)
-
-void ordenamientoPorInsercionNombreApellido(char arregloChar[], int validos){
-
-    FILE * archi=fopen(arregloChar, "r+b");
-    stCliente cliente;
-    int j=1;
-    int i=0-1;
-    int flag=0;
-
-    char auxCliente[30];
-    char aux2Cliente[30];
-    char auxNombre[30];
-    char auxNombre2[30];
-    char auxApellido[30];
-    char auxApellido2[30];
-    char auxAuxNombre[30];
-    char auxAuxApellido[30];
-
-
-
-    if(archi!=NULL){
-
-        while((fread(&cliente, sizeof(stCliente), 1, archi))>0 && j<validos+1){
-
-
-            do{
-
-            flag=0;
-
-            strcpy(auxNombre, cliente.nombre);
-            strcpy(auxApellido, cliente.apellido);
-
-            unirApellidoNombre(cliente, auxCliente);
-
-            fseek(archi, sizeof(stCliente) * i+1, SEEK_SET);
-            fread(&cliente, sizeof(stCliente), 1, archi);
-
-            strcpy(auxNombre2, cliente.nombre);
-            strcpy(auxApellido2, cliente.apellido);
-
-            unirApellidoNombre(cliente, aux2Cliente);
-
-            if(strcmpi(auxCliente, aux2Cliente)==(0-1)){
-
-                strcpy(auxAuxNombre, auxNombre2);
-                strcpy(auxAuxApellido, auxApellido2);
-
-                strcpy(cliente.nombre, auxNombre);
-                strcpy(cliente.apellido, auxApellido);
-
-                i--;
-
-                fseek(archi, sizeof(stCliente) * i, SEEK_SET);
-
-                strcpy(cliente.nombre, auxNombre2);
-                strcpy(cliente.apellido, auxApellido2);
-                flag=1;
-
-            }else{
-
-                fseek(archi, sizeof(stCliente) * j, SEEK_SET);
-
-            }
-
-            }while(flag!=0);
-
-            i=j-1;
-            j++;
-
-        }
-
-    fclose(archi);
-
-    }else{
-
-        printf("No se pudo abrir el archivo\n");
-
-    }
-
-
-}
-
-void unirApellidoNombre(stCliente cliente, stCliente auxCliente[]){
-
-    strcat(auxCliente, cliente.nombre);
-    strcat(auxCliente, cliente.apellido);
-
-}
